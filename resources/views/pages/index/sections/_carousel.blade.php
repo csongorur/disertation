003270a62e757f3ac729df2eba3b4dbdddf609ff
Ur Csongor-Levente <csongor.ur@c4studio.ro>
2018-06-17 13:51:36 +0300

    design update

--- resources/views/pages/index/sections/_carousel.blade.php
+++ resources/views/pages/index/sections/_carousel.blade.php
@@ -5,11 +5,11 @@
                 <div class="item">
                     <div class="col-12 col-lg-6">
                         <img class="img-fluid" src="{{ asset('images/carousel/girl.png') }}" alt="" />
-                        <a class="btn d-block d-lg-none mobile-btn" href="">{{ __('Discover') }}</a>
+                        <a class="btn d-block d-lg-none mobile-btn" href="{{ route('shop') }}">{{ __('Discover') }}</a>
                     </div>
                     <div class="col-12 col-lg-6">
                         <h2 class="d-none d-lg-block">{{ __('Watches') }}</h2>
-                        <a class="btn d-none d-lg-inline-block" href="">{{ __('Discover') }}</a>
+                        <a class="btn d-none d-lg-inline-block" href="{{ route('shop') }}">{{ __('Discover') }}</a>
                     </div>
                 </div>
             </div>
